eab397d8c579855903f45cf0c2fb755f7a404c41
Stariel Isaac <starielfernandes@gmail.com>
2022-08-22 19:13:32 -0300

    resolução de exercícios

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio_8.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio_8.js
@@ -1,1 +1,5 @@
 //Utilizando for, crie um array que vá de 1 até 25 e imprima o resultado;
+
+for(let index = 0; index <= 25; index+=1){
+    console.log(index)
+}
--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio_9.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicio_9.js
@@ -1,1 +1,5 @@
 //Utilizando o array criado no exercício anterior imprima o resultado da divisão de cada um dos elementos por 2.
+
+for(let index = 0; index <= 25; index+=1){
+    console.log(index / 2)
+}gi
