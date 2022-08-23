68eb25d8e6faed8a5bea1ed433247ca32cfaa359
Stariel Isaac <starielfernandes@gmail.com>
2022-08-23 14:53:15 -0300

    resolução de exercícios

--- 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio_3.js
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio_3.js
@@ -3,9 +3,9 @@ let maior = array[0]
 let menor = array[0]
 
 for(let index = 0; index < array.length; index+=1){
-   if(maior < array[index]) {
+   if(maior.length < array[index].length) {
     maior = array[index]
-   } if (menor > array[index]) {
+   } if (menor.length > array[index].length) {
     menor = array[index]    
    }  
 }
---
+++ 01-fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicio_5_bonus.js
@@ -0,0 +1,11 @@
+
+
+let symbol = '*';
+let add = '';
+
+for (let index = 0; index < 5 ; index += 1) {
+  add = add + symbol;
+}
+for (let index = 0; index < 5; index += 1) {
+  console.log(add);
+}
