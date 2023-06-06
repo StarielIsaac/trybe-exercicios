18683ac1b3c5c6d18cfd44d8803e9a9298966d33
Stariel Isaac <starielisaac@gmail.com>
2023-06-06 13:31:02 -0300

    feat: implemented indexError

---
+++ 04-ciências-da-computação/Introdução à Python/Aprendendo Python/1-erros-comuns/1-IndexError.py
@@ -0,0 +1,12 @@
+lista_frutas = ["banana", "uva", "maca", "pera"]
+print(lista_frutas)
+
+# 1) Para cada fruta na lista, imprima a primeira letra da fruta
+for fruta in lista_frutas:
+    print(fruta[0])
+print('-----')
+# 2) Para cada fruta na lista, imprima a quarta letra da fruta
+for fruta in lista_frutas:
+    print(fruta[3])
+
+# if(len(fruta) > 3):
