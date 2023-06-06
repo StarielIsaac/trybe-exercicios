a06ad697ddba0a17083cccad74a4b675325c0b57
Stariel Isaac <starielisaac@gmail.com>
2023-06-06 13:33:19 -0300

    feat: implemented nameError

---
+++ 04-ciências-da-computação/Introdução à Python/Aprendendo Python/1-erros-comuns/3-NameError.py
@@ -0,0 +1,11 @@
+# 1- Imprima uma variável não declarada
+x = 'Will'
+print(x)
+
+meu_dict = {123: "um dois tres", "chave": "valor"}
+
+# # 2- Imprima uma chave existente
+print(meu_dict[123])
+
+# # 3- Imprima uma chave inexistente
+print(meu_dict[variavel_z])
