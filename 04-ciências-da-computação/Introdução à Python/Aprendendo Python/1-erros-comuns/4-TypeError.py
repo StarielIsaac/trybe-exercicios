4164c897c4dd2c29f4b5a7785db7616ba7c42eb6
Stariel Isaac <starielisaac@gmail.com>
2023-06-06 13:34:01 -0300

    feat: implemented typeError

---
+++ 04-ciências-da-computação/Introdução à Python/Aprendendo Python/1-erros-comuns/4-TypeError.py
@@ -0,0 +1,6 @@
+lista_dois = ["dois", "dois", "dos", 2]
+tupla_dois = tuple(lista_dois)
+
+# Use len para cada elemento da tupla confira se funciona para string e inteiro
+for item in tupla_dois:
+    print(len(item))
