358ec45e73ce8988c01d05d3813687ee911adbe3
Stariel Isaac <starielisaac@gmail.com>
2023-06-16 14:28:58 -0300

    feat: implemented problem_answer_2

---
+++ 04-ciências-da-computação/Algoritmos/01-Complexidade de Algoritmos/desafio-numero-a-direita/problem_answer_4.py
@@ -0,0 +1,33 @@
+""""
+###PROBLEMA:
+Dada uma lista de números inteiros, de tamanho n,
+percorrer cada elemento da lista e substituí-lo pelo maior valor
+que estiver à sua direita na lista. Caso não exista um valor maior à direita
+do elemento, o elemento original deve ser mantido.
+"""
+
+DESCRIPTION = "Dois loops com segunde range otimizado"
+TIME_COMPLEXITY = "O(n^2)"
+SPACE_COMPLEXITY = "O(1)"
+
+
+def number_substitution(numbers: list[int]):
+    for i in range(len(numbers)):
+        original_element = numbers[i]
+        right_max = original_element
+
+        for j in range(i + 1, len(numbers)):
+            new_element = numbers[j]
+            if new_element > right_max:
+                right_max = new_element
+            numbers[i] = right_max
+
+
+if __name__ == "__main__":
+    numbers_list = [2, 1, 2, 3, 0, 5, 1, 2, 3]  # 5
+    #   resposta = [5, 5, 5, 5, 5, 5, 3, 3, 3]
+    # saída =      []
+
+    print("Antes:", numbers_list)
+    number_substitution(numbers_list)
+    print("Depois:", numbers_list)
