381de3810dcfcd4ca0dd2d878c62be2e8461c2d5
Stariel Isaac <starielisaac@gmail.com>
2023-06-20 22:34:16 -0300

    feat: implemented class motorized_equipment

---
+++ 04-ciências-da-computação/Estrutura de Dados I: Listas Lineares/elevator_system/motorized_equipment.py
@@ -0,0 +1,18 @@
+import time
+from elevator_system.motor import Motor, MotorSide
+
+
+class MotorizedEquipment:
+    def __init__(self, motor=Motor):
+        self.motor = motor
+
+    def forward(self, seconds):
+        self.motor.turn_on(MotorSide.CLOCKWISE)
+        time.sleep(seconds)
+        self.motor.turn_off()
+
+    # sentido anti-horário
+    def backward(self, seconds):
+        self.motor.turn_on(MotorSide.ANTICLOCKWISE)
+        time.sleep(seconds)
+        self.motor.turn_off()
